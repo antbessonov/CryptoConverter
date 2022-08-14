3c2959cd4812a4584eee141436a491f61a6806af
antbessonov <anton@bessonov.org>
2022-08-14 16:18:27 +0700

    Переименование MainActivity.kt в CoinPriceListActivity.kt

---
+++ app/src/main/java/com/example/cryptoconverter/presentation/CoinPriceListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cryptoconverter.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cryptoconverter.R
+
+class CoinPriceListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_price_list)
+    }
+}
