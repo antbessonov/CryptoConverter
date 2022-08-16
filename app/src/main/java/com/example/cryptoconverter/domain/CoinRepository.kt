7769d37d915bbeafc558ebc1ed3c8eb8ed438903
antbessonov <anton@bessonov.org>
2022-08-16 15:45:59 +0700

    Создание LoadDataUseCase.kt

--- app/src/main/java/com/example/cryptoconverter/domain/CoinRepository.kt
+++ app/src/main/java/com/example/cryptoconverter/domain/CoinRepository.kt
@@ -7,4 +7,6 @@ interface CoinRepository {
     fun getCoinInfoList(): LiveData<List<CoinInfo>>
 
     fun getCoinInfo(id: Int): LiveData<CoinInfo>
+
+    suspend fun loadData()
 }
---
+++ app/src/main/java/com/example/cryptoconverter/domain/LoadDataUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.cryptoconverter.domain
+
+class LoadDataUseCase(
+    private val repository: CoinRepository,
+) {
+
+    suspend operator fun invoke() = repository.loadData()
+}
