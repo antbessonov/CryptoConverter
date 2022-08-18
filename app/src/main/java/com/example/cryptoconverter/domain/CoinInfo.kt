c69411b87899de4cb92278bbca874e5d72b68d0a
antbessonov <anton@bessonov.org>
2022-08-18 15:00:47 +0700

    Изменение полей CoinInfo

--- app/src/main/java/com/example/cryptoconverter/domain/CoinInfo.kt
+++ app/src/main/java/com/example/cryptoconverter/domain/CoinInfo.kt
@@ -4,7 +4,7 @@ data class CoinInfo(
     val id: String?,
     val name: String?,
     val symbol: String?,
-    val tags: String?,
     val lastUpdated: String?,
     val price: String?,
+    val url: String?,
 )
