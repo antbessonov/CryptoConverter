b38a2801175e75d65f20d0e8441b8e0f57900b58
antbessonov <anton@bessonov.org>
2022-08-18 23:03:35 +0700

    Изменение в CoinInfoAdapter.kt

--- app/src/main/java/com/example/cryptoconverter/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/com/example/cryptoconverter/presentation/adapters/CoinInfoAdapter.kt
@@ -1,13 +1,17 @@
 package com.example.cryptoconverter.presentation.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import com.example.cryptoconverter.R
 import com.example.cryptoconverter.databinding.ItemCoinInfoBinding
 import com.example.cryptoconverter.domain.CoinInfo
 import com.squareup.picasso.Picasso
 
-class CoinInfoAdapter : ListAdapter<CoinInfo, CoinInfoViewHolder>(CoinInfoDiffCallback) {
+class CoinInfoAdapter(
+    private val context: Context,
+) : ListAdapter<CoinInfo, CoinInfoViewHolder>(CoinInfoDiffCallback) {
 
     var onCoinClickListener: OnCoinClickListener? = null
 
@@ -22,10 +26,12 @@ class CoinInfoAdapter : ListAdapter<CoinInfo, CoinInfoViewHolder>(CoinInfoDiffCa
 
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
         val coin = getItem(position)
+        val lastUpdateTemplate = context.resources.getString(R.string.last_update_template)
+        Picasso.get().load(coin.url).into(holder.binding.ivLogoCoin)
         holder.binding.tvSymbolCoin.text = coin.symbol
         holder.binding.tvNameCoin.text = coin.name
         holder.binding.tvPriceCoin.text = coin.price
-        Picasso.get().load(coin.url).into(holder.binding.ivLogoCoin)
+        holder.binding.tvLastUpdated.text = String.format(lastUpdateTemplate, coin.lastUpdated)
     }
 
     interface OnCoinClickListener {
