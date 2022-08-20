5908b24eb3ab3baa8925bfbb1e5d2c7cff35da2f
antbessonov <anton@bessonov.org>
2022-08-20 21:20:04 +0700

    Вывод полученого запроса по API в Log

--- app/src/main/java/com/example/cryptoconverter/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoconverter/presentation/CoinPriceListActivity.kt
@@ -1,11 +1,16 @@
 package com.example.cryptoconverter.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoconverter.data.network.APIFactory
 import com.example.cryptoconverter.databinding.ActivityCoinPriceListBinding
 import com.example.cryptoconverter.domain.CoinInfo
 import com.example.cryptoconverter.presentation.adapters.CoinInfoAdapter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class CoinPriceListActivity : AppCompatActivity() {
 
@@ -16,6 +21,8 @@ class CoinPriceListActivity : AppCompatActivity() {
     private val viewModel by lazy {
         ViewModelProvider(this)[CoinViewModel::class.java]
     }
+
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +42,23 @@ class CoinPriceListActivity : AppCompatActivity() {
             }
         }
 
+        val disposable = APIFactory.apiService.getCoinPriceConversion(amount = 100)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            }, {
+                Log.d("TEST_OF_LOADING_DATA", it.message.toString())
+            })
+        compositeDisposable.add(disposable)
+
         viewModel.coinInfoList.observe(this) {
             adapter.submitList(it)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
