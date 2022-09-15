c48dac7be97730dfb811c1d6a1705386d43066f3
antbessonov <anton@bessonov.org>
2022-09-15 11:33:41 +0700

    Редактирование CoinConvertScreen.kt

--- app/src/androidTest/java/com/example/cryptoconverter/kaspresso/screen/CoinConvertScreen.kt
+++ app/src/androidTest/java/com/example/cryptoconverter/kaspresso/screen/CoinConvertScreen.kt
@@ -8,13 +8,6 @@ import com.example.cryptoconverter.R
 
 object CoinConvertScreen : Screen<CoinConvertScreen>() {
 
-    val logo = KImageView { withId(R.id.iv_logo_coin) }
-    val symbol = KTextView { withId(R.id.tv_symbol_coin) }
-    val name = KTextView { withId(R.id.tv_name_coin) }
-    val price = KTextView { withId(R.id.tv_price_coin) }
-    val currencyConvert = KTextView { withId(R.id.tv_currency_convert) }
-    val lastUpdated = KTextView { withId(R.id.tv_last_updated) }
-
     val amountCurrencyToConvert = KEditText { withId(R.id.edt_amount_currency_to_convert) }
     val currencyConvertEdt = KTextView { withId(R.id.tv_currency_convert_edt) }
     val convertResult = KTextView { withId(R.id.tv_convert_result) }
